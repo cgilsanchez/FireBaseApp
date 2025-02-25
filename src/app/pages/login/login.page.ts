31697a31617282c12c972305e03071296784cdd4
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-25 11:55:26 +0100

    directiva y pipe

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,16 +4,19 @@ import { RouterModule } from '@angular/router'; // Importa RouterModule
 import { IonicModule } from '@ionic/angular'; // Importa IonicModule
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { PasswordVisibilityPipe } from '../../pipes/password-visibility.pipe'; // Importa el Pipe aquí
+import { HoverColorDirective } from 'src/app/directives/hover-color.directive';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true, // Configura el componente como standalone
-  imports: [ReactiveFormsModule, RouterModule, IonicModule], // Asegúrate de incluir RouterModule aquí
+  imports: [ReactiveFormsModule, RouterModule, IonicModule,PasswordVisibilityPipe,HoverColorDirective], // Asegúrate de incluir RouterModule aquí
 })
 export class LoginPage {
   loginForm: FormGroup;
+  isPasswordVisible: boolean = false; // Controla la visibilidad de la contraseña
 
   constructor(
     private fb: FormBuilder,
@@ -30,12 +33,12 @@ export class LoginPage {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       try {
-        await this.authService.login(email, password); // Intenta iniciar sesión
+        await this.authService.login(email, password);
         console.log('Inicio de sesión exitoso');
-        this.router.navigate(['/home']); // Redirige a la pantalla de creación de personas
+        this.router.navigate(['/home']);
       } catch (error) {
-        console.error('Error en el inicio de sesión:');
-        alert('Error al iniciar sesión: '); // Muestra un mensaje de error al usuario
+        console.error('Error en el inicio de sesión:', error);
+        alert('Error al iniciar sesión.');
       }
     }
   }
