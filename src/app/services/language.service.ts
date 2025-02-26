bd0839b66e1793465e6c7847f5b87270eecd9643
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-26 12:17:25 +0100

    t

---
+++ src/app/services/language.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  private language = new BehaviorSubject<string>('es'); // Idioma por defecto
+  private translations: any = {};
+
+  constructor(private http: HttpClient) {
+    this.loadTranslations(this.language.value);
+  }
+
+  setLanguage(lang: string) {
+    this.language.next(lang);
+    this.loadTranslations(lang);
+  }
+
+  private loadTranslations(lang: string) {
+    this.http.get(`/assets/i18n/${lang}.json`).subscribe(translations => {
+      this.translations = translations;
+    });
+  }
+
+  translate(key: string): string {
+    return this.translations[key] || key; // Si no se encuentra, devuelve la clave original
+  }
+}
