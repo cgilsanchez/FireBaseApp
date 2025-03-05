71baae90005c4cd948a1ac689400567e3c59ddc6
cgilsanchez <cgilsanchez.02@gmail.com>
2025-03-06 00:40:50 +0100

    plugin de share

--- src/app/components/receta-detalle/receta-detalle.component.ts
+++ src/app/components/receta-detalle/receta-detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-receta-detalle',
@@ -16,5 +17,18 @@ export class RecetaDetalleComponent {
 
   close(): void {
     this.modalController.dismiss();
+  }
+  async shareReceta() {
+    if (!this.receta) {
+      console.error('No hay receta disponible para compartir');
+      return;
+    }
+
+    await Share.share({
+      title: `Receta: ${this.receta.titulo}`,
+      text: `🍽️ ¡Mira esta receta deliciosa de ${this.receta.titulo}! \n\n📝 Ingredientes: ${this.receta.ingredientes.join(', ')}\n\n📖 Instrucciones: ${this.receta.descripcion}`,
+      url: `http://localhost:4200/recetas/${this.receta.id}`,
+      dialogTitle: 'Compartir Receta'
+    });
   }
 }
